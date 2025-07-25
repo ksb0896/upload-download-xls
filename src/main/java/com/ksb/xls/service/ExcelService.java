2845db6e9e7057e4166f980fcc7394959ded795d
ksb0896 <186450057+ksb0896@users.noreply.github.com>
2025-07-25 17:59:37 +0530

    bussiness logic of GET,POST api --done

--- src/main/java/com/ksb/xls/service/ExcelService.java
+++ src/main/java/com/ksb/xls/service/ExcelService.java
@@ -1,4 +1,41 @@
 package com.ksb.xls.service;
 
+import com.ksb.xls.helper.ExcelHelper;
+import com.ksb.xls.model.xls;
+import com.ksb.xls.repo.xlsRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.List;
+
+@Service
 public class ExcelService {
+    @Autowired
+    xlsRepo repo;
+
+    //saving the xls file - POST call
+    public void save(MultipartFile file){
+    try{
+        List<xls> xls = ExcelHelper.excelToXls(file.getInputStream());
+        repo.saveAll(xls);
+    } catch (IOException e) {
+        throw new RuntimeException("Fail to store excel file: " +e.getMessage());
+    }
+    }
+
+    //get all xls - GET - get all excel file
+    public List<xls> getAllExcelFile(){
+        return repo.findAll();
+    }
+
+    //load the excel(with data) - GET - download
+    public ByteArrayInputStream load(){
+        List<xls> xls = repo.findAll();
+
+        ByteArrayInputStream inputStream = ExcelHelper.xlsToExcel(xls);
+        return inputStream;
+    }
 }
