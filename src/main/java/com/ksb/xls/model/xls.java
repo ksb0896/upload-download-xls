0d1758cf407fb56a550409f7eea20da56a154a2e
ksb0896 <186450057+ksb0896@users.noreply.github.com>
2025-07-25 14:50:04 +0530

    lombok -added, database structure -created

--- src/main/java/com/ksb/xls/model/xls.java
+++ src/main/java/com/ksb/xls/model/xls.java
@@ -1,4 +1,34 @@
 package com.ksb.xls.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "xls")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class xls  {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Id")
+    private long id;
+
+    @Column(name = "Title")
+    private String title;
+
+    @Column(name = "Description")
+    private String description;
+
+    @Column(name = "Published")
+    private boolean published;
+
+    // this specific method will run and give a formatted string
+    @Override
+    public String toString(){
+        return "Excel [id=" + id + ", title=" + title + ", desc=" + description + ", published=" + published + "]";
+    }
 }
