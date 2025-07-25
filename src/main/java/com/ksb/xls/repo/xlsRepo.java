4fb95a690dee198498f2e70fd63d6a8eb3e33b44
ksb0896 <186450057+ksb0896@users.noreply.github.com>
2025-07-25 16:59:28 +0530

    repo layer --added

--- src/main/java/com/ksb/xls/repo/xlsRepo.java
+++ src/main/java/com/ksb/xls/repo/xlsRepo.java
@@ -1,4 +1,7 @@
 package com.ksb.xls.repo;
 
-public interface xlsRepo {
+import com.ksb.xls.model.xls;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface xlsRepo extends JpaRepository<xls, Long> {
 }
